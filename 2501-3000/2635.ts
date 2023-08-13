af397014c803c7fe197df5ee9e0efcd7c892be3f
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-08-13 14:31:59 +0530

    Create 2635.ts

---
+++ 2501-3000/2635.ts
@@ -0,0 +1,7 @@
+function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+    const result: number[] = []
+    for (let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i))
+    }
+    return result
+}
