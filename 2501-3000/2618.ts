93a5aecafd0d094a6529b3aa7a102a4b019997dd
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-08-13 14:19:24 +0530

    Create 2618.ts

---
+++ 2501-3000/2618.ts
@@ -0,0 +1,26 @@
+/**
+ * @param {Object} object
+ * @param {Function} classFunction
+ * @return {boolean}
+ */
+function checkIfInstanceOf(obj: any, classFunction: any): boolean {
+    if (obj === null || obj === undefined || classFunction === null || classFunction === undefined) {
+        return false
+    }
+
+    let prototype = Object.getPrototypeOf(obj)
+
+    while (prototype != null) {
+        if (prototype.constructor === classFunction) {
+            return true
+        }
+        prototype = Object.getPrototypeOf(prototype)
+    }
+
+    return false
+}
+
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
