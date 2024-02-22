6a6802c02c0133538b9d95f5df7b5fc6f77021f6
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2024-02-22 10:42:28 +0530

    Create 997.ts

---
+++ 501-1000/997.ts
@@ -0,0 +1,12 @@
+// TS
+function findJudge(N: number, trust: number[][]): number {
+  const array = new Array(N).fill(0);
+
+  trust.forEach(([a, b]) => {
+    array[a - 1]--;
+    array[b - 1]++;
+  });
+
+  const judge = array.indexOf(N - 1);
+  return judge !== -1 ? judge + 1 : -1;
+}
